fa7a47f4383ba422d4fb6f70219c5a491c8ba6ae
Himanshu Kumar <himanshuishwar399@gmail.com>
2024-09-29 16:42:54 +0530

    Todo App

---
+++ 10todolocalstroage/src/contexts/index.js
@@ -0,0 +1,6 @@
+// import { todoContext,useTodo, TodoProvider } from "./todoContext";
+
+
+// export {todoContext, useTodo, TodoProvider}
+
+export {todoContext, TodoProvider, useTodo} from "./todoContext"
---
+++ 10todolocalstroage/src/contexts/todoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+ export  const todoContext = createContext({
+    todos:[
+        {
+            id: 1,
+            title: "Buy milk",
+            completed: false
+    }],
+    addTodo: (todo) => {},
+    deleteTodo: (id) => {},
+    updateTodo: (id, todo) => {},
+    toggleComplete: (id)=>{}
+ });
+
+
+ export const useTodo = ()=> {
+    return useContext(todoContext);
+ }
+
+
+ export const TodoProvider = todoContext.Provider;
+
